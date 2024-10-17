8d58e4836be366f4bda823f5fb52352f5f64707b
VladimirPoliak <vladimir.poliak@torontomu.ca>
2024-10-17 18:13:27 -0400

    Updated Styling

--- Final_Project/main.js
+++ Final_Project/main.js
@@ -1,9 +1,9 @@
-function transformScroll(event) {
+//https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal
+
+function verticalToHorizontal(event) {
     event.preventDefault();
     window.scroll(event.currentTarget.scrollLeft += event.deltaY, 0);
   }
   
-  var element = document.scrollingElement;
-  element.scrollTop= 5;
-  element.addEventListener('wheel', transformScroll, {passive: false});
+document.scrollingElement.addEventListener('wheel', verticalToHorizontal, {passive: false});
 
