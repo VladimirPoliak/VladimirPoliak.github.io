5c31899cf6f198795faac861ccc9096a385b51a6
VladimirPoliak <vladimir.poliak@torontomu.ca>
2024-10-29 17:23:39 -0400

    Added images and fixed links.

--- Final_Project/main.js
+++ Final_Project/main.js
@@ -10,33 +10,3 @@ function verticalToHorizontal(event) {
 
 //This scrollingElement eventlistner detects movement in the mousewheel and executes the verticalToHorizontal function.
 document.scrollingElement.addEventListener('wheel', verticalToHorizontal, {passive: false});
-
-/* This function styles the HTML element with the specified ID with 
-an increased size for when the picture is hovered. */
-function enlarge(id) {
-  var pictureChange = document.getElementById(id);
-  pictureChange.style.width = "60%";
-  pictureChange.style.height = "60%";
-  pictureChange.style.position = "absolute";
-}
-
-/* This function styles the HTML element with the specified ID with 
-an removed size modification for when the picture is unhovered. */
-function shrink(id) {
-  var pictureChange = document.getElementById(id);
-  pictureChange.style.width = null;
-  pictureChange.style.height = null;
-  pictureChange.style.position = null;
-}
-
-
-/* When clicking anywhere on the website besides the menu button, the event is detected as not pressing the menu button, so the
-content is grabbed in a variable and then it toggles the "show" condition from all of the variables in the menu making them disappear.
-Followed ITM780 Lab 5 Instructions to achive.
-window.onclick = function(event) {
-  if (!event.target.matches('clickable')) {
-    var picture = document.getElementById("clickable");
-    picture.style.width = null;
-    picture.style.height = null;
-    } 
-  }*/
