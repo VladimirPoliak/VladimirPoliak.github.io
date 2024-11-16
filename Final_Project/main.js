d1d355a5c14f3c7e56e7ecc8d0c5a9d5b2d0f738
VladimirPoliak <vladimir.poliak@torontomu.ca>
2024-11-15 21:56:11 -0500

    Fixed comments.js

--- Final_Project/main.js
+++ Final_Project/main.js
@@ -1,8 +1,6 @@
-//https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal
-
-
 /* This function uses the preventDefault method to stop the website from scrolling normally.
-The window.scroll method translates the vertical movement of the mouse wheel to horizontal page movement. */
+The window.scroll method translates the vertical movement of the mouse wheel to horizontal page movement. 
+The tutorial followed to create this function is https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal*/
 function verticalToHorizontal(event) {
     event.preventDefault();
     window.scroll(event.currentTarget.scrollLeft += 3*event.deltaY, 0);
@@ -11,10 +9,11 @@ function verticalToHorizontal(event) {
 //This scrollingElement eventlistner detects movement in the mousewheel and executes the verticalToHorizontal function.
 document.scrollingElement.addEventListener('wheel', verticalToHorizontal, {passive: false});
 
-// This confirmation function prompts the user to confirm they want to leave the website, so they can choose not to if they clicked by mistake.
+/* This confirmation function prompts the user to confirm they want to leave the website, so they can choose not to if they clicked by mistake.
+The tutorial followed to create this function is //https://developer.mozilla.org/en-US/docs/Web/API/Window/open*/
 function confirmation(link) {
   var confirmation = confirm("This Link Goes to An External Website\nPress 'Ok' If You want to Go.");
   if (confirmation == true) {
-    window.open(link, "_blank") //https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+    window.open(link, "_blank")
   }
 }
